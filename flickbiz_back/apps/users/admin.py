051f62f2e374bf10bcafc3f0290a24327da85970
B. <bd90962@gmail.com>
2026-05-06 14:11:15 +0200

    feat(models): register all models in Django admin

--- flickbiz_back/apps/users/admin.py
+++ flickbiz_back/apps/users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
 # Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ["username", "email", "country", "is_private"]
+    fieldsets = UserAdmin.fieldsets + (
+        ("Profile", {"fields": ("profile_picture", "country", "bio", "is_private", "hide_watchlist", "hide_favorites", "preferred_genres")}),
+    )
