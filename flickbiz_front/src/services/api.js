8eb5ee62d37236628a7764762f0eb30c66f702e8
B. <bd90962@gmail.com>
2026-06-06 01:57:27 +0200

    fix: add api prefix to base URL

--- flickbiz_front/src/services/api.js
+++ flickbiz_front/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000/api",
-  withCredentials: true,
+  baseURL: "https://flickbiz-back.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
