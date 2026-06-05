cc3fd5b3f83d34a947afd54ce704463e889a00f5
B. <bd90962@gmail.com>
2026-06-05 16:24:33 +0200

    feat(notifications): add notifications page and backend endpoints (pending)

--- flickbiz_back/apps/notifications/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Notification
-
-# Register your models here.
-@admin.register(Notification)
-class NotificationAdmin(admin.ModelAdmin):
-    list_display = ["user", "type", "is_read", "created_at"]
-    list_filter = ["type", "is_read"]
