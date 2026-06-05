595de11104650b9bbdfaab9a4f5d2074fc9d4272
B. <bd90962@gmail.com>
2026-06-06 01:29:48 +0200

    adding external hostnames to allowed hosts

--- flickbiz_back/flickbiz_back/settings.py
+++ flickbiz_back/flickbiz_back/settings.py
@@ -11,6 +11,10 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "dev-insecure-key")
 DEBUG = os.environ.get("DEBUG", "True") == "True"
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost").split(",")
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
