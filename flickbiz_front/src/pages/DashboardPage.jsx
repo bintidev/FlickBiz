698109f69d80dab13380a22542b072ef19022a1a
B. <bd90962@gmail.com>
2026-06-10 15:56:55 +0200

    fix uncaught reference

--- flickbiz_front/src/pages/DashboardPage.jsx
+++ flickbiz_front/src/pages/DashboardPage.jsx
@@ -397,7 +397,6 @@ export default function DashboardPage(){
   return(
     <div style={{minHeight:"100vh",cursor:"none"}}>
       <DashCanvas/>
-      <Navbar />
 
       {/* Ticker de Estadísticas */}
       <div className="fixed top-[72px] left-0 right-0 z-30 overflow-hidden border-b py-2"
